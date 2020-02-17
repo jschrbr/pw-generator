db36300194fe5591da5150fdfcbe7350608dfa71
jschrbr <schrbrr@gmail.com>
2020-02-17 18:15:49 +1100

    added validate inputs and generate password functions

--- Develop/script.js
+++ Develop/script.js
@@ -6,18 +6,34 @@
 //array of letters, numbers and specials
 // get pwlength
 
-// 
+//
+
+//scratch that ascii 32 -> 126
+// lower 97-122
+// upper 65-90
+// special 33-47,58-64,91-96,123-126
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  return;
+}
+
+function validateInputs() {
+  return true;
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (validateInputs()) {
+    var password = generatePassword();
 
+    passwordText.value = password;
+  } else {
+  }
 }
 
 // Add event listener to generate button
