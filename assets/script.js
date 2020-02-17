a97eae0883710c5ed74787a4371e7b902551fbec
jschrbr <schrbrr@gmail.com>
2020-02-17 23:13:50 +1100

    Improved clarity - call for character ranges in generate password function - returns range
    Range is parsed to getChars function which returns a string based on the selected ranges

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
-var lower = document.querySelector("#customControlValidation1");
-var upper = document.querySelector("#customControlValidation2");
-var numeric = document.querySelector("#customControlValidation3");
-var special = document.querySelector("#customControlValidation4");
-var len = document.querySelector("#validationServer01");
+var lowerCaseCheckBox = document.querySelector("#customControlValidation1");
+var upperCaseCheckBox = document.querySelector("#customControlValidation2");
+var numericCheckBox = document.querySelector("#customControlValidation3");
+var specialCheckBox = document.querySelector("#customControlValidation4");
+var passwordLength = document.querySelector("#validationServer01");
 
 function getChars(range) {
   var charset = "";
@@ -22,43 +22,44 @@ function getChars(range) {
   return charset;
 }
 
-function getCharRanges(chars) {
+function getCharRanges(charTypes) {
   var range = [];
-  if (chars[0]) {
+  if (charTypes[0]) {
     //ASCII a-z
     range.push([97, 122]);
   }
-  if (chars[1]) {
+  if (charTypes[1]) {
     //ASCII A-Z
     range.push([65, 90]);
   }
-  if (chars[2]) {
+  if (charTypes[2]) {
     //ASCII 0-9
     range.push([48, 57]);
   }
-  if (chars[3]) {
-    //ASCII special chars.
+  if (charTypes[3]) {
+    //ASCII specialCheckBox charTypes.
     range.push([33, 47], [58, 64], [91, 96], [123, 126]);
   }
   return range;
 }
 
-function generatePassword(len, chars) {
+function generatePassword(passwordLength, charTypes) {
   var password = "";
-  var range = getCharRanges(chars);
-  var charset = getChars(range);
+  var charset = getChars(getCharRanges(charTypes));
   var i = 0;
 
-  while (i < len) {
+  while (i < passwordLength) {
     var num = Math.floor(Math.random() * charset.length);
     password += charset[num];
     i++;
   }
+
   return password;
 }
 
+// Check if the entered password length is a number and between 8 and 128
 function validateLength(len) {
-  if (!isNaN(len) && len > 7 && len < 129) {
+  if (!isNaN(len) && len >= 8 && len <= 128) {
     return true;
   }
   return false;
@@ -66,12 +67,16 @@ function validateLength(len) {
 
 // Write password to the #password input
 function writePassword() {
-  var chars = [lower.checked, upper.checked, numeric.checked, special.checked];
-
-  if (!(chars[0] || chars[1] || chars[2] || chars[3])) {
+  var charTypes = [
+    lowerCaseCheckBox.checked,
+    upperCaseCheckBox.checked,
+    numericCheckBox.checked,
+    specialCheckBox.checked
+  ];
+  if (!(charTypes[0] || charTypes[1] || charTypes[2] || charTypes[3])) {
     alert("Fail - check");
-  } else if (validateLength(len.value)) {
-    var password = generatePassword(len.value, chars);
+  } else if (validateLength(passwordLength.value)) {
+    var password = generatePassword(passwordLength.value, charTypes);
     passwordText.value = password;
   } else {
     alert("Fail - password length");
